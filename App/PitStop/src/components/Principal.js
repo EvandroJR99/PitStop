af492067bb9feecb41718dc1b04e246f83cd44e0
Amélia Alice <alice.cardoso10@gmail.com>
2018-03-09 12:45:01 -0500

    PitStopApp
    Mudanças no componente "principal.js"

--- App/PitStop/src/components/Principal.js
+++ App/PitStop/src/components/Principal.js
@@ -23,40 +23,56 @@ const Principal = props => (
                 </View>
             </View>
         </View>
-        <View style={{ flex: 1, padding: 50, flexDirection: 'column', justifyContent: 'center' }}>
-            <View >
-                <TouchableOpacity onPress={() => Actions.automoveis()}>
-                    <View style={{
-                        backgroundColor: '#F9A825', alignItems: 'center',
-                        justifyContent: 'center', borderRadius: 2, height: 50, elevation: 4, marginBottom: 6
-                    }}
-                    >
-                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}>AUTOMÓVEIS</Text>
-                    </View>
-                </TouchableOpacity>
-
+        <View style={{ flex: 1, padding: 50, justifyContent: 'center' }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <View style={{ paddingRight: 20  }}>
+                    <TouchableOpacity onPress={() => Actions.automoveis()}>
+                        <View style={{
+                            backgroundColor: '#F9A825', alignItems: 'center',
+                            justifyContent: 'center', borderRadius: 2, width: 115, height: 121, elevation: 4, marginBottom: 6
+                        }}
+                        >
+                            <Icon name="car" size={45} color="#fff" />
+                            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, paddingTop: 10 }}>AUTOMÓVEIS</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity >
+                        <View style={{
+                            backgroundColor: '#F9A825', alignItems: 'center',
+                            justifyContent: 'center', borderRadius: 2, width: 115, height: 121, elevation: 4, marginBottom: 6
+                        }}
+                        >   
+                            <Icon name="map-marker" size={45} color="#fff" />
+                            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, paddingTop: 10 }}>LOCAIS</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={{ paddingTop: 20 }}>
-                <TouchableOpacity >
-                    <View style={{
-                        backgroundColor: '#F9A825', alignItems: 'center',
-                        justifyContent: 'center', borderRadius: 2, height: 50, elevation: 4, marginBottom: 6
-                    }}
-                    >
-                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}>ESTABELECIMENTOS</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            <View style={{ paddingTop: 20 }}>
-                <TouchableOpacity >
-                    <View style={{
-                        backgroundColor: '#F9A825', alignItems: 'center',
-                        justifyContent: 'center', borderRadius: 2, height: 50, elevation: 4, marginBottom: 6
-                    }}
-                    >
-                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}>INTERVENÇÕES</Text>
-                    </View>
-                </TouchableOpacity>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', paddingTop: 15 }}>
+                <View style={{ paddingRight: 20  }}>
+                    <TouchableOpacity >
+                        <View style={{
+                            backgroundColor: '#F9A825', alignItems: 'center',
+                            justifyContent: 'center', borderRadius: 2, width: 115, height: 121, elevation: 4, marginBottom: 6
+                        }}
+                        >
+                            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}>INTERVENÇÕES</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <TouchableOpacity >
+                        <View style={{
+                            backgroundColor: '#F9A825', alignItems: 'center',
+                            justifyContent: 'center', borderRadius: 2, width: 115, height: 121, elevation: 4, marginBottom: 6
+                        }}
+                        >
+                            <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}>SOBRE</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     </View>
