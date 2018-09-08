3e4a0248fe298d39002424a8fa28720bb2d1bc4c
Evandro Cavalcante <evd99.jr@gmail.com>
2018-09-08 17:27:06 -0500

    Remove pasta App

--- App/IHM-PitStop/PitStop1/src/actions/validations.js
+++
@@ -1,4 +0,0 @@
-const soletras = /[a-zA-Z\u00C0-\u00FF ]+/i;
-const validationEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-export { soletras, validationEmail };
--- App/PitStop1/index.android.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-
-const PitStop1 = props => (
-  <App />
-)
-
-AppRegistry.registerComponent('PitStop1', () => PitStop1);
